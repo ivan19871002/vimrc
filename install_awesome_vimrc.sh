fc4480cdc4ef940a89fa76ae976176d4bdce7892
huangqiwu <huangqiwu@xiaomi.com>
2015-06-29 10:52:57 +0800

    Revert "for dotfiles"
    This reverts commit 432611b310949fd1380c69ecb0500b67dabfac4a.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,14 +1,14 @@
-cd ~/.dotfiles/vimrc
+cd ~/.vim_runtime
 
-echo 'set runtimepath+=~/.dotfiles/vimrc
+echo 'set runtimepath+=~/.vim_runtime
 
-source ~/.dotfiles/vimrc/vimrcs/basic.vim
-source ~/.dotfiles/vimrc/vimrcs/filetypes.vim
-source ~/.dotfiles/vimrc/vimrcs/plugins_config.vim
-source ~/.dotfiles/vimrc/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
 
 try
-source ~/.dotfiles/vimrc/my_configs.vim
+source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
